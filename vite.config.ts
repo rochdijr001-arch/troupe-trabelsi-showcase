4c7f52fee0cd9d9a602cf4523d19645976ea2fbf
unknown <rochdijr001@gmail.com>
2026-05-10 12:16:49 +0100

    fix: add tailwindcss plugin to resolve unstyled layout

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
   ],
 });
